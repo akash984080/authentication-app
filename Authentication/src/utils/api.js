cd81b56477ba7e67a9403599f46b59eca1a0a347
akash984080 <akash984080@gmail.com>
2025-11-24 18:43:37 +0530

    url changed

--- Authentication/src/utils/api.js
+++ Authentication/src/utils/api.js
@@ -1,7 +1,10 @@
-
 const BASE_URL = import.meta.env.MODE === "development"
-  ? import.meta.env.VITE_BASE_URL
-  : import.meta.env.VITE_API_URL;
+  ?"http://localhost:8080/api/auth"
+  : "https://your-backend.up.railway.app/api";
+
+  VITE_BASE_URL=
+VITE_API_URL=
+
 
 export async function apiPost(path, data) {
   const res = await fetch(`${BASE_URL}${path}`, {
