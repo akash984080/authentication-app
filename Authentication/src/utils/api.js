e9b7b88a7396ad193cdcf5984b2d32c599cfd5a7
akash984080 <akash984080@gmail.com>
2025-11-24 18:47:53 +0530

    url changed

--- Authentication/src/utils/api.js
+++ Authentication/src/utils/api.js
@@ -1,23 +1,19 @@
 const BASE_URL = import.meta.env.MODE === "development"
-  ?"http://localhost:8080/api/auth"
-  : "https://your-backend.up.railway.app/api";
+  ? import.meta.env.VITE_BASE_URL
+  : import.meta.env.VITE_API_URL;
 
-  VITE_BASE_URL=
-VITE_API_URL=
-
-
-export async function apiPost(path, data) {
+export async function apiPost (path, data) {
   const res = await fetch(`${BASE_URL}${path}`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  });
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  })
 
-  const body = await res.json();
+  const body = await res.json()
 
   if (!res.ok) {
-    throw { status: res.status, body };
+    throw { status: res.status, body }
   }
 
-  return body;
+  return body
 }
